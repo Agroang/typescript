1a277eb5a463a8e8af31f2fc4a9e08ceee6fccd6
matias acuna <agroang@gmail.com>
2021-12-22 09:34:22 +0900

    added a typescript file and a comment with an example on how to assign types in typescript

---
+++ typescript-101/index.ts
@@ -0,0 +1,7 @@
+// assigning types in typescript
+let age: number = 34
+let firstName: string = 'Angela'
+let busy: boolean = true
+
+// what the above means is that now age can only be a number, firstname a string
+// and busy a boolean, this is how you assign types in typescript
