c65e643c3f907a16c491c6bb2ddf88f42dd5b652
matias acuna <agroang@gmail.com>
2022-01-02 21:31:14 +0900

    started trying to conditional render the questionnaire

--- react-apps/first-react-ts-app/src/components/Landing.tsx
+++ react-apps/first-react-ts-app/src/components/Landing.tsx
@@ -1,7 +1,22 @@
 import * as React from 'react';
+import Questionnaire from './components/Questionnaire';
+
+// have a true false state, on click change state to true, if true
+// conditional display the questionnaire component
+// the other stuff is rendered if false!
 
 export default function Landing() {
+  const [pageTrigger, setPageTrigger] = React.useState(false)
+
+  function handleClick() {
+    setPageTrigger(true)
+  }
+
   return (
-    <p>test</p>
+    <div>
+      <h1>El Questionator</h1>
+      <p>The hardest and awesomest questions ever!</p>
+      <button onClick={handleClick}>Start Quizzing</button>
+    </div>
   )
 }
---
+++ react-apps/first-react-ts-app/src/components/Questionnaire.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function Questionnaire() {
+  return (
+    <div>
+      Multiple Questions here
+    </div>
+  )
+}
