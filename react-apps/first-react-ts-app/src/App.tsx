f4960315448c4f3adf223c6ec6a26444d42b821a
matias acuna <agroang@gmail.com>
2022-01-02 21:19:49 +0900

    import React in TS done for first landing component

--- react-apps/first-react-ts-app/src/App.tsx
+++ react-apps/first-react-ts-app/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Landing from './components/Landing';
 
 /* <img src={logo} className="App-logo" alt="logo" /> */
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>test</p>
+        <Landing />
       </header>
     </div>
   );
---
+++ react-apps/first-react-ts-app/src/components/Landing.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default function Landing() {
+  return (
+    <p>test</p>
+  )
+}
